bb50c889f821801a576e6d631a8120eaeccc7a3e
HenriqueNeubert <neubert.study@gmail.com>
2024-02-08 23:44:18 -0300

    feat: Working with prompt

--- curso-luis-otavio/assets/index.js
+++ curso-luis-otavio/assets/index.js
@@ -1,1 +1,7 @@
+const testingConfirm = confirm("Confirmado?")
+const testingPrompt = prompt("Digite aqui!")
+
 console.log("hello World!!!");
+console.log(testingConfirm);
+console.log(testingPrompt);
+
