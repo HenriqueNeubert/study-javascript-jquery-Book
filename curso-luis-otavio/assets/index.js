ca9958132b105c5c10809dad9e31d5b7ee86f586
HenriqueNeubert <neubert.study@gmail.com>
2024-02-09 00:28:50 -0300

    feat: challenge (let)

--- curso-luis-otavio/assets/index.js
+++ curso-luis-otavio/assets/index.js
@@ -1,13 +1,34 @@
-const testingConfirm = confirm("Confirmado?")
-let num1 = prompt("Digite aqui!")
-let num2 = prompt("Digite aqui!")
+// const testingConfirm = confirm("Confirmado?")
+// let num1 = prompt("Digite aqui!")
+// let num2 = prompt("Digite aqui!")
 
-console.log("hello World!!!");
-console.log(testingConfirm);
+// console.log("hello World!!!");
+// console.log(testingConfirm);
 
-num1 = Number(num1);
-num2 = Number(num2);
-const result = String(num1 + num2)
+// num1 = Number(num1);
+// num2 = Number(num2);
+// const result = String(num1 + num2)
 
-console.log(result);
+// console.log(result);
 
+/////////////////////////////
+
+let varA = 'A' //b
+let varB = 'B' //c
+let varC = 'C' //a
+//
+// varAtemp = varA
+// varBtemp = varB
+// varCtemp = varC
+
+// varA = varBtemp
+// varB = varCtemp
+// varC = varAtemp
+
+//OR
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC);
+
+console.log(typeof(varC))
