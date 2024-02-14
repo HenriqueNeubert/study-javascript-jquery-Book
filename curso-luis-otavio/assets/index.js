0cb2c51169bf61642cb8a103a1759bcbfb606a77
HenriqueNeubert <neubert.study@gmail.com>
2024-02-14 17:42:04 -0300

    feat: working with isNan, toString, isInteger, toFixed

--- curso-luis-otavio/assets/index.js
+++ curso-luis-otavio/assets/index.js
@@ -48,3 +48,19 @@
 // console.log(nameUser.indexOf('u')); //procura pelo index
 // console.log(nameUser.match(/[a-z]/g));
 // console.log(nameUser.search(/i/g)); //pesquisa por param
+
+
+let number1 = 15.243432
+let number2 = 2.5
+
+console.log(number1.toString() + number2);
+console.log(number1.toString(2)); //Representação binária
+console.log(number1.toFixed(2));//Arredondar (2 casas)
+console.log(Number.isInteger(number1));// verifica se o número é inteiro
+
+let temp = number1 * 's'
+console.log(Number.isNaN(temp)); //verifica se a conta é valida via tru/false
+
+num1 = parseFloat(number1.toFixed(2)) // Transforma em float
+console.log(num1);
+console.log(Number.isSafeInteger(num1));
