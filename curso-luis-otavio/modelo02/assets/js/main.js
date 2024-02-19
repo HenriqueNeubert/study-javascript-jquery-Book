62739ca8dce47f45dfda77694be8c178445a8e22
HenriqueNeubert <neubert.study@gmail.com>
2024-02-19 15:20:12 -0300

    feat: add imc message

--- curso-luis-otavio/modelo02/assets/js/main.js
+++ curso-luis-otavio/modelo02/assets/js/main.js
@@ -1,7 +1,7 @@
 let inputWeigth = document.querySelector('#weigth')
 let inputHeigth = document.querySelector('#heigth')
 let mainFormIMC = document.querySelector('#mainFormIMC')
-
+let messageResult = document.querySelector('#messageResult')
 
 function eventForm(event) {
     event.preventDefault();
@@ -17,19 +17,20 @@ mainFormIMC.addEventListener("submit", eventForm);
 
 
 function calcIMC(imc){
-
+    
     if (imc < 18.5) {
         console.log(`Abaixo do Peso: IMC = ${imc.toFixed(2)}`);
     }else if(imc >= 18.5 && imc <= 24.9){
-        console.log(`Peso Normal: IMC = ${imc.toFixed(2)}`);
+        messageResult.innerHTML += `<p>Peso Normal: IMC = ${imc.toFixed(2)}</p>`
     }else if(imc >= 25 && imc <= 29.9){
-        console.log(`Sobrepeso: IMC = ${imc.toFixed(2)}`);
+        messageResult.innerHTML += `<p>Sobrepeso: IMC = ${imc.toFixed(2)}</p>`
     }else if(imc >= 30 && imc <= 34.9){
-        console.log(`Obesidade Grau 1: IMC = ${imc.toFixed(2)}`);
+        messageResult.innerHTML += `<p>Obesidade Grau 1: IMC = ${imc.toFixed(2)}</p>`
     }else if (imc >= 35 && imc <= 39.9){
-        console.log(`Obesidade Grau 2: IMC = ${imc.toFixed(2)}`);
+        messageResult.innerHTML += `<p>Obesidade Grau 2: IMC = ${imc.toFixed(2)}</p>`
     }else if (imc >= 40){
-        console.log(`Obesidade Grau 3: IMC = ${imc.toFixed(2)}`);
+        messageResult.innerHTML += `<p>Obesidade Grau 3: IMC = ${imc.toFixed(2)}</p>`
+
     }
 
     return imc
