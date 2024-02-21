24816714963265d4da8fe24df9795af4a5178368
HenriqueNeubert <neubert.study@gmail.com>
2024-02-21 10:56:50 -0300

    feat: working with Date()

--- curso-luis-otavio/modelo02/assets/js/aulas.js
+++ curso-luis-otavio/modelo02/assets/js/aulas.js
@@ -8,3 +8,43 @@ function ternaria(){
 }
 
 // ternaria()
+
+function data(){
+    // // const data = new Date()
+    // // console.log(data.toString()); 
+                        //a, m, d, M, s, ms
+    function teste(){
+        const data = new Date(2019, 3, 20, 15,14,27)
+        console.log('Dia', data.getDate());
+        console.log('Dia', data.getMonth());//mês começa do 0
+        console.log('Ano', data.getFullYear());
+        console.log('Dia semana', data.getDay()); //0 = domingo e 6 = sábado
+        //...
+    }
+    //teste()
+
+    function zeroAEsquerda (num){
+        return num >= 10 ? num : `0${num}`
+    }
+
+    function formataData(data){
+        const dia = zeroAEsquerda(dataAtual.getDate())
+        const mes = zeroAEsquerda(dataAtual.getMonth() +1)
+        const ano = zeroAEsquerda(dataAtual.getFullYear())
+        const hora = zeroAEsquerda(dataAtual.getHours)
+        const min = zeroAEsquerda(dataAtual.getMinutes)
+        const seg = zeroAEsquerda(dataAtual.getSeconds)
+
+        return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+    }
+    const dataAtual = new Date()
+    dataBrasil = formataData(dataAtual)
+    console.log(dataAtual)
+}
+data()
+
+
+function switchAndCase(){
+
+}
+switchAndCase()
