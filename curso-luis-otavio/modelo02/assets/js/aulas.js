ce33b032c2e35b1c853d39c0cb1c87d1e8155a9b
HenriqueNeubert <neubert.study@gmail.com>
2024-02-25 00:07:20 -0300

    feat: for exercises

--- curso-luis-otavio/modelo02/assets/js/aulas.js
+++ curso-luis-otavio/modelo02/assets/js/aulas.js
@@ -237,8 +237,28 @@ function forLoop(){
   const letras = ['a','b', 'c']
 
   for(let i = 0; i < letras.length; i++){
-    console.log(letras[i], - i);
+    // console.log(letras[i], - i);
   }
+
+  const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'},
+  ]
+
+  const containerExercise = document.querySelector('.containerExercise')
+  const div = document.createElement('div')
+
+  for(let i = 0; i< elementos.length; i++){
+    let { tag, texto } = elementos[i]
+    let tagCriada = document.createElement(tag)
+    tagCriada.innerHTML = texto
+    div.appendChild(tagCriada)
+  }
+
+  containerExercise.appendChild(div)
+  //REFAZER ESTE EXERCICIO
 
 }
 forLoop()
